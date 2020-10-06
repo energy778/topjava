eff0ad2dab3191a73a61b1ff69701dcc34034ac7
energy 778 <one.million.dollars@mail.ru>
2020-10-06 22:59:30 +0300

    1.2. show meals list in MealServlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,17 +1,22 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
+import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_MEALS;
 
 public class MealServlet extends HttpServlet {
-
     private static final Logger log = getLogger(MealServlet.class);
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -21,7 +26,9 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-        response.sendRedirect("meals.jsp");
-    }
+        List<MealTo> mealTOs = MealsUtil.filteredByStreams(DEFAULT_MEALS, LocalTime.MIN, LocalTime.MAX, DEFAULT_CALORIES_PER_DAY);
 
+        request.setAttribute("meals", mealTOs);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+    }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,11 +1,36 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html lang="ru">
+
 <head>
     <title>Meals</title>
+    <link rel="stylesheet" href="css/meals.css"/>
 </head>
+
 <body>
+
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+
+<table border="1">
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    </thead>
+    <c:forEach items="${meals}" var="meal">
+        <tr class="${meal.excess ? 'excess' : 'notExcess'}">
+            <td>${meal.dateTime.toLocalDate()} ${meal.dateTime.toLocalTime()}</td>
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+        </tr>
+    </c:forEach>
+</table>
+
 </body>
+
 </html>
