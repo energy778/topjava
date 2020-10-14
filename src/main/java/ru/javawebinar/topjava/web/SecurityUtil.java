32c2f35d1c0bef8ee7febe60ef54b3d9d60b2fae
energy 778 <one.million.dollars@mail.ru>
2020-10-14 04:24:52 +0300

    Optional. add authenticate mock on the view

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -4,11 +4,18 @@ import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
+    private static int authUserId;
+
     public static int authUserId() {
-        return 1;
+        return authUserId;
+    }
+
+    public static void setAuthUserId(int newAuthUserId) {
+        authUserId = newAuthUserId;
     }
 
     public static int authUserCaloriesPerDay() {
         return DEFAULT_CALORIES_PER_DAY;
     }
+
 }
